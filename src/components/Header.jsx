9699fc7c97df24fa47ce40e00ffd6bf4b0ee10ca
Festus891 <festus891@yahoo.com>
2023-12-05 12:29:58 -1100

    work in progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,9 +30,18 @@ function Header() {
       setIsMenu(!isMenu);
     }
   };
+  const Logout = () => {
+    setIsMenu(false);
+    localStorage.clear();
+
+    dispatch({
+      type: actionType.SET_USER,
+      user: null,
+    });
+  };
 
   return (
-    <header className="fixed z-50 w-screen  p-6 px-16">
+    <header className="fixed z-50 w-screen  p-3 px-4 md:px-16">
       {/* desktop and tablet */}
       <div className="hidden md:flex w-full h-full items-center justify-between">
         <Link to={"/"} className="flex items-center gap-2">
@@ -40,7 +49,12 @@ function Header() {
           <p className="text-headingColor text-xl font-bold">city</p>
         </Link>
         <div className="flex items-center gap-8">
-          <ul className="flex items-center gap-8  cursor-pointer">
+          <motion.ul
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 200 }}
+            className="flex items-center gap-8  cursor-pointer"
+          >
             <li className="text-base text-textColor hover:text-headingColor duration-100 ease-in-out transition-all  cursor-pointer">
               Home
             </li>
@@ -53,7 +67,7 @@ function Header() {
             <li className="text-base text-textColor hover:text-headingColor duration-100 ease-in-out transition-all  cursor-pointer">
               Service
             </li>
-          </ul>
+          </motion.ul>
           <div className="relative flex items-center justify-center ">
             {/* <MdShoppingCart /> */}
             <MdShoppingBasket className="text-textColor text-2xl  cursor-pointer" />
@@ -95,7 +109,67 @@ function Header() {
       </div>
 
       {/* mobile */}
-      <div className="flex md:hidden w-full h-full"></div>
+      <div className="flex  items-center justify-between md:hidden w-full h-full">
+        <div className="relative flex items-center justify-center ">
+          {/* <MdShoppingCart /> */}
+          <MdShoppingBasket className="text-textColor text-2xl  cursor-pointer" />
+          <div className="absolute -top-2 -right-2  w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+            <p className="text-xs text-white font-semibold">2</p>
+          </div>
+        </div>
+
+        <Link to={"/"} className="flex items-center gap-2">
+          <img src={logo} alt="logo" className="w-8 object-cover" />
+          <p className="text-headingColor text-xl font-bold">city</p>
+        </Link>
+
+        <div className="relative">
+          {" "}
+          <motion.img
+            whileTap={{ scale: 0.6 }}
+            src={user ? user.photoURL : avatar}
+            className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+            alt="user_avatar"
+            onClick={login}
+          />
+          {isMenu && (
+            <motion.div
+              initial={{ opacity: 0, scale: 0.6 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.6 }}
+              className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
+            >
+              {user && user.email === "festus4537@gmail.com" && (
+                <Link to={"/createItem"}>
+                  <p className="px-4 py-2 flex items-center gap 3 cursor-pointer hover:bg-slate-100 transition-all duration-200 ease-in-out text-textColor text-base  ">
+                    New item <MdAdd className="pl-1" />
+                  </p>
+                </Link>
+              )}
+              <ul className="flex flex-col    cursor-pointer">
+                <li className="text-base text-textColor  hover:text-headingColor duration-100 ease-in-out transition-all px-4 py-2 cursor-pointer hover:bg-slate-100">
+                  Home
+                </li>
+                <li className="text-base text-textColor hover:text-headingColor duration-100 ease-in-out transition-all px-4 py-2 cursor-pointer hover:bg-slate-100">
+                  Menu
+                </li>
+                <li className="text-base text-textColor hover:text-headingColor duration-100 ease-in-out transition-all px-4 py-2 cursor-pointer hover:bg-slate-100">
+                  About Us
+                </li>
+                <li className="text-base text-textColor hover:text-headingColor duration-100 ease-in-out transition-all px-4 py-2 cursor-pointer  hover:bg-slate-100">
+                  Service
+                </li>
+              </ul>
+              <p
+                className="m-2 p-2 rounded-md shadow-md flex items-center gap 3 cursor-pointer justify-center bg-gray-200  hover:bg-gray-300 transition-all duration-200 ease-in-out text-textColor text-base  "
+                onClick={Logout}
+              >
+                Logout <MdLogout className="pl-1" />
+              </p>
+            </motion.div>
+          )}
+        </div>
+      </div>
     </header>
   );
 }
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import delivery from "../img/delivery.png";
 
 function MainContainer() {
-  return <div>MainContainer</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap2">
+      <div className=" py-2 flex-1 flex flex-col items-start md:items-center justify-center">
+        <div className="flex items-center gap-2 justify-center bg-orange-800">
+          <p>Bike delivery</p>
+          <div className="w-10 h-10 bg-white rounded-full overflow-hidden">
+            <img
+              src={delivery}
+              alt="delivery"
+              className="w-full h-full object-contain"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="py-2 bg-blue-400 flex-1"></div>
+    </div>
+  );
 }
 
 export default MainContainer;
