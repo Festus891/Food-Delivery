44dc022ac02402bff566787fd323eb37f7e7ef84
Aderibigbe Festus Adewale <FESTUS891@YAHOO.COM>
2024-01-02 13:44:55 -1100

    updating homepage

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,22 +1,11 @@
 import React from "react";
-import delivery from "../img/delivery.png";
+
+import HomeContainer from "./HomeContainer";
 
 function MainContainer() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap2">
-      <div className=" py-2 flex-1 flex flex-col items-start md:items-center justify-center">
-        <div className="flex items-center gap-2 justify-center bg-orange-800">
-          <p>Bike delivery</p>
-          <div className="w-10 h-10 bg-white rounded-full overflow-hidden">
-            <img
-              src={delivery}
-              alt="delivery"
-              className="w-full h-full object-contain"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="py-2 bg-blue-400 flex-1"></div>
+    <div className=" w-full h-auto flex flex-col items-center justify-center">
+      <HomeContainer />
     </div>
   );
 }
---
+++ src/utills/data.js
@@ -0,0 +1,35 @@
+import I1 from "../img/i1.png";
+import f1 from "../img/f1.png";
+import c3 from "../img/c3.png";
+import fi1 from "../img/fi1.png";
+
+export const heroData = [
+  {
+    id: 1,
+    name: "Icecream",
+    decp: "Chocolate & vanila",
+    price: "5.25",
+    imageSrc: I1,
+  },
+  {
+    id: 2,
+    name: "Strawberries",
+    decp: "fresh strawberries",
+    price: "10.25",
+    imageSrc: f1,
+  },
+  {
+    id: 3,
+    name: "Chicken kebab",
+    decp: "Mixed Chicken",
+    price: "17.25",
+    imageSrc: c3,
+  },
+  {
+    id: 4,
+    name: "Fish",
+    decp: "Mixed fish kebab",
+    price: "8.25",
+    imageSrc: fi1,
+  },
+];
